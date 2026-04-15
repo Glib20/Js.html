a1fdcc68b167f6dd485ef0d9ea05853a4f1e2975
Glib20 <zvermedick3@gmail.com>
2026-04-15 20:05:12 +0300

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,29 @@
+let age = 13;
+console.log(age);
+
+let name = "Гліб";
+console.log(name);
+
+let isStudent = true;
+console.log(isStudent);
+
+let myString = "У всьому є своя краса, але не кожен може її побачити";
+console.log(myString);
+
+let myNumber = 25;
+myNumber += 10;
+console.log(myNumber);
+
+let myNull = null;
+console.log(myNull);
+
+let userName = prompt("Введіть своє ім’я");
+alert(userName);
+console.log(typeof userName);
+
+let userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+console.log(userAnswer);
+
+alert("Увага! Дія може бути небезпечною.");
+let confirmDanger = confirm("Ви впевнені, що хочете продовжити?");
+console.log(confirmDanger);
